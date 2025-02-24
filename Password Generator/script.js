2c888737bf55f8cece60f59d663a0b0d538b6c54
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-02-24 23:44:18 +0600

    Create Password Generator and Cart UI

---
+++ Password Generator/script.js
@@ -0,0 +1,38 @@
+const passwordBox = document.getElementById ('password');
+const length = 20;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const number = '123456789';
+const symbol = '@!#$%^&*/';
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+
+function createPassword () {
+    let password = '';
+    password += upperCase[Math.floor (Math.random () * upperCase.length)];
+    password += lowerCase[Math.floor (Math.random () * lowerCase.length)];
+    password += number[Math.floor (Math.random () * number.length)];
+    password += symbol[Math.floor (Math.random () * symbol.length)];
+    
+    while (length > password.length) {
+        password += allChars [Math.floor (Math.random () * allChars.length)]; 
+    } 
+    passwordBox.value = password;
+}
+
+function copyPassword () {
+    passwordBox.select ();
+    document.execCommand ('copy');
+}
+
+
+// window.addEventListener("resize", function () {
+//     let borderDiv = document.querySelector(".border-div");
+//     if (window.innerHeight < 500) { // Approx small height detect
+//         borderDiv.style.height = "100vh";
+//     } else {
+//         borderDiv.style.height = "90vh";
+//     }
+// });
