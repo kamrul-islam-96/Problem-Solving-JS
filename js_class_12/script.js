ee1fad91fd07780b3ef09303096b8ced39d36279
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-02-25 21:49:26 +0600

    cls 12 html and js comment out

--- js_class_12/script.js
+++ js_class_12/script.js
@@ -1,91 +1,91 @@
 // One
-// var selectSpan = document.querySelector('span');
-// var count = 1;
+var selectSpan = document.querySelector('span');
+var count = 1;
 
-// function callingCount () {
-//     count++;
-//     selectSpan.textContent = count;
-// }
+function callingCount () {
+    count++;
+    selectSpan.textContent = count;
+}
 
 
 
 // Two
-// var countN = document.getElementById ('count');
-// var increaseDecreaceCount = Number (countN.innerText);
+var countN = document.getElementById ('count');
+var increaseDecreaceCount = Number (countN.innerText);
 
-// function increaceCount () {
-//     countN.textContent = increaseDecreaceCount;
-//     increaseDecreaceCount++;
-// }
+function increaceCount () {
+    countN.textContent = increaseDecreaceCount;
+    increaseDecreaceCount++;
+}
 
-// function decreaceCount () {
-//     countN.textContent = increaseDecreaceCount;
-//     increaseDecreaceCount--;
-// }
+function decreaceCount () {
+    countN.textContent = increaseDecreaceCount;
+    increaseDecreaceCount--;
+}
 
 
 
 // Three
-// var count = document.getElementById ('count');
-// var increaseDecreaceCount = Number (count.innerHTML);
-// var notice = document.getElementById ('notice')
+var count = document.getElementById ('count');
+var increaseDecreaceCount = Number (count.innerHTML);
+var notice = document.getElementById ('notice')
 
-// function increaceCount () {
-//     if (increaseDecreaceCount === 5) {
-//         notice.innerHTML = 'You can not purchase more than 5 items';
-//         setTimeout(clearNotice, 2000);
-//         return;
-//     }
-//     increaseDecreaceCount++;
-//     count.textContent = increaseDecreaceCount;
-//     clearNotice ();
-// }
+function increaceCount () {
+    if (increaseDecreaceCount === 5) {
+        notice.innerHTML = 'You can not purchase more than 5 items';
+        setTimeout(clearNotice, 2000);
+        return;
+    }
+    increaseDecreaceCount++;
+    count.textContent = increaseDecreaceCount;
+    clearNotice ();
+}
 
-// function decreaceCount () {
-//     if (increaseDecreaceCount === 1) {
-//         notice.innerHTML = 'Cart item can not be less than one';
-//         setTimeout(clearNotice, 2000);
-//         return;
-//     }
-//     increaseDecreaceCount--;
-//     count.textContent = increaseDecreaceCount;
-//     clearNotice ();
-// }
+function decreaceCount () {
+    if (increaseDecreaceCount === 1) {
+        notice.innerHTML = 'Cart item can not be less than one';
+        setTimeout(clearNotice, 2000);
+        return;
+    }
+    increaseDecreaceCount--;
+    count.textContent = increaseDecreaceCount;
+    clearNotice ();
+}
 
-// function clearNotice () {
-//     notice.innerHTML = '';
-// }
-
-
-
-// var count = document.getElementById ('count');
-// var increaseDecreaceCount = Number (count.innerHTML);
-// var notice = document.getElementById ('notice');
+function clearNotice () {
+    notice.innerHTML = '';
+}
 
 
-// function increaceCount () {
-//     if (increaseDecreaceCount === 5) {
-//         notice.innerHTML = 'You can not purchase more than 5 items'
-//         setTimeout (clearNotice, 2000)
-//         return;
-//     }
-//     count.innerHTML = increaseDecreaceCount;
-//     increaseDecreaceCount++;
-//     clearNotice ();
-// }
 
-// function decreaceCount () {
-//     if (increaseDecreaceCount === 1) {
-//         notice.innerHTML = 'cart item can not be less than one'
-//         setTimeout (clearNotice, 2000)
-//         return;
-//     }
-//     count.innerHTML = increaseDecreaceCount;
-//     increaseDecreaceCount--;
-//     clearNotice ();
-// }
+var count = document.getElementById ('count');
+var increaseDecreaceCount = Number (count.innerHTML);
+var notice = document.getElementById ('notice');
 
 
-// function clearNotice () {
-//     notice.innerHTML = "";
+function increaceCount () {
+    if (increaseDecreaceCount === 5) {
+        notice.innerHTML = 'You can not purchase more than 5 items'
+        setTimeout (clearNotice, 2000)
+        return;
+    }
+    count.innerHTML = increaseDecreaceCount;
+    increaseDecreaceCount++;
+    clearNotice ();
+}
+
+function decreaceCount () {
+    if (increaseDecreaceCount === 1) {
+        notice.innerHTML = 'cart item can not be less than one'
+        setTimeout (clearNotice, 2000)
+        return;
+    }
+    count.innerHTML = increaseDecreaceCount;
+    increaseDecreaceCount--;
+    clearNotice ();
+}
+
+
+function clearNotice () {
+    notice.innerHTML = "";
 }
